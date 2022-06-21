5dcd06dafb135fc1fad83d17c19fc5c1172e0bf3
Vipin Kumar Kashyap <vipinkashyap2110@gmail.com>
2022-06-21 13:07:35 -0700

    Revert "Adding a deleting example"
    This reverts commit 3fd31c1fc04bc499e1a36184f957fa2ecf49fd1c.

--- flutter_bloc_delete_demo/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ flutter_bloc_delete_demo/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/kashyap/FlutterDev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/kashyap/Documents/GitHub/flutter_bloc_delete_demo"
+export "FLUTTER_APPLICATION_PATH=/Users/kashyap/Documents/GitHub/Learning-Flutter/flutter_bloc_delete_demo"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
